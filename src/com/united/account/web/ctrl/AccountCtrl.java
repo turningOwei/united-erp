db130814984be7a46fac59654dd90202061e643b
turningOwei <175408322@qq.com>
2016-12-17 15:47:54 +0800

    加入parent

--- src/com/united/account/web/ctrl/AccountCtrl.java
+++ src/com/united/account/web/ctrl/AccountCtrl.java
@@ -1,5 +1,6 @@
 package com.united.account.web.ctrl;
 
+import com.common.SystemConfig;
 import com.united.account.dao.entity.Account;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
