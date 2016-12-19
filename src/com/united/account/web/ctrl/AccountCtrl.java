b927a0dd3ecb84ae481ecfdee3ff6bf818af3d0d
turningOwei <175408322@qq.com>
2016-12-19 22:05:05 +0800

    配合前端分页完成

--- src/com/united/account/web/ctrl/AccountCtrl.java
+++ src/com/united/account/web/ctrl/AccountCtrl.java
@@ -1,10 +1,19 @@
 package com.united.account.web.ctrl;
 
-import com.common.SystemConfig;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.page.PageParam;
 import com.united.account.dao.entity.Account;
+import com.united.account.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.portlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by turningOwei on 2016/11/30.
@@ -12,10 +21,28 @@ import org.springframework.web.portlet.ModelAndView;
 @Controller
 @RequestMapping("/account")
 public class AccountCtrl {
+    @Autowired
+    private AccountService accountService;
+
     @RequestMapping("/login.do")
     public ModelAndView login(Account account){
         ModelAndView mv = new ModelAndView("index");
         mv.addObject("ctx","testCtx");
         return mv;
+    }
+
+    @RequestMapping("/listByPage.do")
+    @ResponseBody
+    public String listByPage(Account account,PageParam<Account> pageParam){
+        List<Account> list = accountService.listByPage(account);
+        Integer totalCount =  accountService.listByPageCount(account);
+        pageParam.setList(list);
+        Gson gson = new GsonBuilder().serializeNulls().create();
+        Map map = new HashMap();
+        map.put("data",list);
+        map.put("totalCount",totalCount);
+        map.put("success",true);
+        String result = gson.toJson(map);
+        return result;
     }
 }
