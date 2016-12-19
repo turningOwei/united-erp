500451e8a316a68a2f87fde38d697715bf0f0b86
turningOwei <175408322@qq.com>
2016-12-19 22:05:20 +0800

    配合前端分页完成

--- src/com/united/account/service/AccountService.java
+++ src/com/united/account/service/AccountService.java
@@ -3,6 +3,8 @@ package com.united.account.service;
 import com.united.account.dao.entity.Account;
 import com.united.permission.dao.entity.Resource;
 
+import java.util.List;
+
 /**
  * Created by turningOwei on 2016/12/8.
  */
@@ -10,4 +12,7 @@ public interface AccountService {
 
     public Resource getResourceByAccount(Account account);
 
+    List<Account> listByPage(Account account);
+
+    Integer listByPageCount(Account account);
 }
