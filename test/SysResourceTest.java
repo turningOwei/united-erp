10ed2b75cd9aafd3e2a6641bc85f57227f2fc5aa
turningOwei <175408322@qq.com>
2017-01-09 16:50:59 +0800

    测试用例

--- test/SysResourceTest.java
+++ test/SysResourceTest.java
@@ -1,29 +1,23 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.united.permission.dao.entity.SysResource;
 import com.united.permission.service.SysResourceService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.annotation.Resource;
-
 /**
- * Created by turningOwei on 2016/12/8.
+ * Created by turningOwei on 2017/1/9.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:config/spring/spring.xml"})
 public class SysResourceTest {
-    private static Logger log = org.slf4j.LoggerFactory.getLogger(SysResourceTest.class);
-    @Resource
+    @Autowired
     private SysResourceService sysResourceService;
+
     @Test
     public void test(){
-        SysResource rootNode = sysResourceService.getRootNode();
-        Gson gson = new GsonBuilder().serializeNulls().create();
-        String str = gson.toJson(rootNode);
-        log.info(str);
+        SysResource res = sysResourceService.getRootNode();
+        System.out.println(res.getText());
     }
 }
