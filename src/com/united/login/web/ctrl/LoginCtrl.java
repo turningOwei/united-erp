b4f2361cfd621ade7ae993db03cceb35a522068f
turningOwei <175408322@qq.com>
2017-01-11 21:03:25 +0800

    设置公司Id

--- src/com/united/login/web/ctrl/LoginCtrl.java
+++ src/com/united/login/web/ctrl/LoginCtrl.java
@@ -4,8 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.united.account.dao.entity.Account;
 import com.united.account.service.AccountService;
+import com.united.corp.dao.entity.Company;
+import com.united.corp.service.CompanyService;
 import com.united.login.pojo.IndexResult;
-import com.united.permission.dao.entity.Resource;
 import com.united.permission.dao.entity.SysResource;
 import com.united.permission.service.ResourceService;
 import com.united.permission.service.SysResourceService;
@@ -15,6 +16,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.portlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by turningOwei on 2016/12/1.
@@ -28,17 +31,40 @@ public class LoginCtrl {
     private AccountService accountService;
     @Autowired
     private ResourceService resourceService;
+    @Autowired
+    private CompanyService companyService;
+
+    @RequestMapping("/login.do")
+    public ModelAndView login(){
+        ModelAndView mv = new ModelAndView("view/login");
+        return mv;
+    }
+
 
     @RequestMapping("/index.do")
-    public ModelAndView login(Account account,ModelMap modelMap,ServletWebRequest request){
+    public ModelAndView index(Account account,ModelMap modelMap,ServletWebRequest request){
         ModelAndView mv = new ModelAndView("view/index");
 
+
+        //设置全局变量
+        //公司code
+        String corpCode = "123";
+        Company corp = companyService.getByCorpCode(corpCode);
+        modelMap.put("corp",corp);
+        modelMap.put("corpId",corp.getOid());
+
+
+
+        //默认设置总经理登录
+        account.setOid(10l);
         //判断是否登录
+        Account accountEntity = accountService.getByOId(account.getOid());
 
         String ctx = request.getContextPath();
         modelMap.put("ctx", ctx);
         modelMap.put("staticPath", ctx+"/static");
-        IndexResult result = getResult(account);
+        modelMap.addAttribute("account",account);
+        IndexResult result = getResult(accountEntity);
         Gson gson = new GsonBuilder().serializeNulls().create();
         modelMap.put("result", gson.toJson(result));
         return mv;
@@ -46,7 +72,7 @@ public class LoginCtrl {
 
     private IndexResult getResult(Account account){
         IndexResult result = new IndexResult();
-        SysResource rootNode = sysResourceService.getRootNode();
+        SysResource rootNode = sysResourceService.getRootNode(account);
        /* Resource rootNode = resourceService.getRootNodeByAccount(account);
         result.setMenuData(rootNode);*/
         result.setMenuData(rootNode);
