71ae8f7d6bafbb7939b55a5dc6eb706c952be3cf
turningOwei <175408322@qq.com>
2017-01-12 15:43:52 +0800

    权限,资源

--- src/com/united/permission/service/SysResourceService.java
+++ src/com/united/permission/service/SysResourceService.java
@@ -1,10 +1,16 @@
 package com.united.permission.service;
 
+import com.united.account.dao.entity.Account;
 import com.united.permission.dao.entity.SysResource;
+
+import java.util.List;
 
 /**
  * Created by turningOwei on 2016/12/8.
  */
 public interface SysResourceService {
-    public SysResource getRootNode();
+    public SysResource getRootNode(Account account);
+
+    public List<SysResource> getListByAccount(Account account);
+
 }
